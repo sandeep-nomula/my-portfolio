8b5f504114168d9bee7c9b32ece388fe690e552e
Sandeep Kumar Nomula <sandykumar.nomula@gmail.com>
2025-04-14 12:56:26 -0400

    updated the url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sandeep-nomula/',
+  base: '/my-portfolio/',
   plugins: [
     react({
       babel: {
